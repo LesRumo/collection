3f7890df713e84cfa9ae3f436927b900ed370aa2
SBICZA01\A253559 <Lesedi.Shirami@standardbank.co.za>
2023-12-19 16:31:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+
+            names.Add("Lee");
+            names.Add("Lesedi");
+            names.Add("Les");
+            names.Add("Sedi");
+
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+            Console.WriteLine(names[3]);
+
+            Console.ReadLine();
+        }
+    }
+}
